a0368b8d6d4e559a73ad4e7c9e1654f1ff5c5fb1
unknown <nita.andu09@gmail.com>
2024-05-14 02:33:16 +0300

    mi e somn

--- Source/Saw.cpp
+++ Source/Saw.cpp
@@ -1,7 +1,7 @@
 #include "../Headers/Saw.h"
 
-Saw::Saw() : FiniteChoice(std::vector<std::pair<float, float>> {{800.f, 420.f}, {500.f, 420.f}}),
-             onGround(true), signFly(-1) {}
+Saw::Saw() : signOnGround(-1), signFly(-1), onGround(true),
+             FiniteChoice(std::vector<std::pair<float, float>> {{800.f, 420.f}, {500.f, 420.f}}) {}
 
 void Saw::spawn() {
     initialPosition = this->choose();
